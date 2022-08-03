9cd9c51741bb4b6719d8100a1f81211c725cb6d7
luizmarcelolm <luizmarcelolm@hotmail.com>
2022-08-03 20:33:50 -0300

    iniciando código

---
+++ javascript.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const imagens = [
+    { 'id': '1', 'url': '' }, { 'id': '2', 'url': '' }, { 'id': '3', 'url': '' }, { 'id': '4', 'url': '' }, { 'id': '5', 'url': '' },
+]
+
+const container =
+
+    const load_imagens = (imagens, container) => {
+
+    }
+
+load_imagens(imagens, container);
