c74a6c59b9f11fb6be5d23b287cac4a8e4d8d6d1
luizmarcelolm <luizmarcelolm@hotmail.com>
2022-08-04 20:41:23 -0300

    iniciando parte 1

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ const imagens = [
     { 'id': '1', 'url': '' }, { 'id': '2', 'url': '' }, { 'id': '3', 'url': '' }, { 'id': '4', 'url': '' }, { 'id': '5', 'url': '' },
 ]
 
-const container =
+const container = document.querySelector('#container_item');
 
-    const load_imagens = (imagens, container) => {
 
-    }
+const load_imagens = (imagens, container) =>
 
-load_imagens(imagens, container);
+
+    load_imagens(imagens, container);
